d6071cdfe8660ef2580b8fd5d746249a050832ff
SvetlanaDanilchyk <danilchyk.svet@gmail.com>
2023-05-24 10:07:16 +0300

    Added task_5 Selenium and XPath

---
+++ WebDriverDevBy/IndexPage.cs
@@ -0,0 +1,51 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace WebDriverDevBy.Pages;
+
+internal class IndexPage
+{
+    IWebDriver _driver;
+    IWebElement _vacancieesLink;
+
+    const string VACANCIES_PAGE_XPATH = "//a[@class='navbar__nav-item navbar__nav-item_label']";
+    const string VACANCIES_COUNT_XPATH = "//h2[@class ='informer__title']";
+
+    public int numberVacancies { get; set; }
+
+    public IndexPage()
+    {
+        _driver = new ChromeDriver();
+        _driver.Url = "https://devby.io/";
+    }
+
+    public void Initialize()
+    {
+        _vacancieesLink = _driver.FindElement(By.XPath(VACANCIES_PAGE_XPATH));
+        GetNumberOfVacancies();
+    }
+
+    public void GetNumberOfVacancies()
+    {
+        var vacanciesCount = _driver.FindElements(By.XPath(VACANCIES_COUNT_XPATH)).First().Text;
+        String[] stringVacanciesCount = vacanciesCount.Split(" ");
+        numberVacancies = Int32.Parse(stringVacanciesCount[0]);
+
+    }
+
+    public VacanciesPage SwitchToVacanciesPage()
+    {
+        _vacancieesLink.Click();
+        new VacanciesPage(_driver).CloseInfoWindowOnVacanciesPage();
+        return new VacanciesPage(_driver);
+
+    }
+
+    public void Unitialize() 
+    {
+        _driver.Close();
+    }
+
+}
+
+
---
+++ WebDriverDevBy/Program.cs
@@ -0,0 +1,35 @@
+﻿using WebDriverDevBy.Pages;
+
+namespace WebDriverDevBy;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        //1 open main page
+        IndexPage indexPage = new IndexPage();
+        indexPage.Initialize();
+
+        //2 switch vacancies page 
+        VacanciesPage vacanciesPage = indexPage.SwitchToVacanciesPage();
+
+        //3 count  all  vacancies on page and print on Console
+        List<CollectionWrapper> collectionWrappers = new List<CollectionWrapper>();
+        collectionWrappers = vacanciesPage.GetCountVacancies();
+        collectionWrappers.ToList().ForEach(x => Console.WriteLine(x));
+
+        Console.WriteLine("\n");
+
+        //4 sor vacancies on page and print on Console
+        List<CollectionWrapper> collectionSortByDescending = vacanciesPage.SortByDescending();
+        collectionSortByDescending.ToList().ForEach(x => Console.WriteLine(x));
+
+        //5 compare by the number of vacancies from the IndexPage
+        Console.WriteLine("\nIndex = " + indexPage.numberVacancies + "\n" + "Vacancies = " + vacanciesPage.numberVacanciesOnVacanciesPage + "\n");
+        Console.WriteLine(indexPage.numberVacancies == vacanciesPage.numberVacanciesOnVacanciesPage);
+
+        indexPage.Unitialize();
+    }
+}
