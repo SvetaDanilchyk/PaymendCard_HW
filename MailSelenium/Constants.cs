2fc97badb643762301f9cfcad29ee2c330e09d10
SvetlanaDanilchyk <danilchyk.svet@gmail.com>
2023-05-25 13:50:08 +0300

    Added task_6 p1-p3

---
+++ MailSelenium/Constants.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSelenium
+{
+    internal class Constants
+    {
+
+        public const string LOGIN_IN_XPATH = "//a[@data-label='header' and text()='Войти']";
+
+        //logIn
+        public const string INPUT_EMAIL_XPATH = "//input[@type='email']";
+        public const string BUTTON_NEXT_XPATH = "//button[.//text()='Далее']";
+        public const string INPUT_PASSWORD_XPATH = "//input[@type='password']";
+
+        //Write
+        public const string BUTTON_WRITE_XPATH = "//div[@class='z0']";
+        public const string INPUT_EMAIL_USER_XPATH = "//input[@role = 'combobox']"; // //input[@id=':ew' ] 
+        public const string INPUT_THEME_XPATH = "//input[@name='subjectbox']";
+        public const string INPUT_MESSAGE_XPATH = "//div[@class='Am Al editable LW-avf tS-tW']";
+        public const string SUBMIT_FORM_LATTER_XPATH = "//div[@class='dC']";
+
+        //logout
+        public const string AKAUNT_GOOGLE_XPATH = "//a[@class = 'gb_d gb_3a gb_v']";
+        public const string LOGOUT_XPATH = "//a[contains(@href,'Logout')]";// "//span[@data-et='10']/child::a"; // "//div[@class = 'SedFmc' and text()='Выйти']";
+
+        
+        //Read
+        public const string OPEN_MESSAGE_XPATH = "tr[@class = 'zA zE x7']";//
+
+        //Reply
+        public const string BUTTON_REPLY_XPATH = "//span[@role = 'link' and text() = 'Ответить']";
+        public const string INPUT_MESSAGE_TO_REPLY = "//div[@class = 'Am aO9 Al editable LW-avf tS-tW']";
+
+        public static string message = "Hello!" + DateTime.Now;
+    }
+}
---
+++ MailSelenium/GmailBasePage.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System.Collections.ObjectModel;
+
+namespace MailSelenium;
+
+public abstract class GmailBasePage
+{
+    protected IWebDriver _driver;
+    protected WebDriverWait _wait;
+     
+    protected GmailBasePage(IWebDriver driver) 
+    {
+            _driver = driver;
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(30));
+    }
+
+    protected IWebElement FindMailElement(string xPath)
+    {
+        return _wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(xPath)));
+    }
+    protected IWebElement FindMessageToRead(string xPath)
+    {
+        WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(600));
+        return wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(xPath)));
+    }
+    protected ReadOnlyCollection <IWebElement> FindMessageElements(string xPath)
+    {
+        return _wait.Until(ExpectedConditions.PresenceOfAllElementsLocatedBy(By.XPath(xPath)));
+    }
+
+
+}
