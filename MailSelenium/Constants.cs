e0733af08fb1ab19f3544bf4191a2400ee3d19e7
SvetlanaDanilchyk <danilchyk.svet@gmail.com>
2023-05-28 21:30:39 +0300

    Refactoring Task_6

--- MailSelenium/Constants.cs
+++ MailSelenium/Constants.cs
@@ -2,24 +2,21 @@
 
 public class Constants
 {
-    //response message 
-    public static string responseMessage = "Hi!" + DateTime.Now;
-
     public const string LOGIN_IN_XPATH = "//a[@data-label='header' and text()='Войти']";
 
     //logIn
-    public const string INPUT_EMAIL_XPATH = "//input[@type='email']";
+    public const string INPUT_EMAIL_XPATH = "//input[@id='identifierId']";
     public const string BUTTON_NEXT_LOGIN_XPATH = "//*[@id='identifierNext']/div/button"; //*[@id="passwordNext"]/div/button "//button[.//text()='Далее']" //*[@id="passwordNext"]/div/button
-    public const string BUTTON_NEXT_PASSWORD_XPATH = "//*[@id='passwordNext']/div/button";
     public const string INPUT_PASSWORD_XPATH = "//input[@name='Passwd']"; //"//input[@type='password']";
+    public const string BUTTON_NEXT_PASSWORD_XPATH = "//*[@id='passwordNext']/div/button";
 
     //Write
-    public const string BUTTON_WRITE_XPATH = "//div[@class='z0']"; // /html/body/div[7]/div[3]/div/div[2]/div[1]/div[1]/div/div
-    public const string INPUT_EMAIL_USER_XPATH = "//div[@aria-label='Строка поиска']/div/input"; //input[@role = 'combobox']   
+    public const string BUTTON_WRITE_XPATH = "//div[text() = 'Написать' or text() = 'Compose']"; // //div[@class='z0']
+    public const string INPUT_EMAIL_USER_XPATH = "//div[@aria-label='Строка поиска' or @aria-label='Search Field']/div/input"; //input[@role = 'combobox']   
     public const string INPUT_THEME_XPATH = "//input[@name='subjectbox']";
-    public const string INPUT_MESSAGE_XPATH = "//div[@class='Am Al editable LW-avf tS-tW']";
-    public const string SUBMIT_FORM_LATTER_XPATH = "//div[@class='dC']";
-    public const string SEND_MESSAG_XPATH = "//*[text()='Сообщение отправлено.']";
+    public const string INPUT_MESSAGE_XPATH = "//div[@aria-label='Текст письма' or @aria-label='Message Body']"; // //div[@class='Am Al editable LW-avf tS-tW']
+    public const string SUBMIT_FORM_LATTER_XPATH = "//div[text() = 'Отправить' or text() = 'Send']";
+    public const string SEND_MESSAG_XPATH = "//*[text()='Сообщение отправлено.' or text() = 'Message sent']";
 
     //logout
     public const string AKAUNT_GOOGLE_XPATH = "//a[@class = 'gb_d gb_3a gb_v']";
@@ -27,18 +24,19 @@ public class Constants
 
     
     //Read
-    public const string OPEN_MESSAGE_XPATH = "tr[@class = 'zA zE x7']";//
+    public const string LIST_OF_UNREAD_MESSAGES = "//tr[@class='zA zE x7' or @class='zA zE' ]";
 
     //Reply
-    public const string BUTTON_REPLY_XPATH = "//span[@role = 'link' and text() = 'Ответить']"; 
+    public const string BUTTON_REPLY_XPATH = "//span[@role = 'link' or text() = 'Ответить']"; 
     public const string INPUT_MESSAGE_TO_REPLY = "//div[@class = 'Am aO9 Al editable LW-avf tS-tW']";
 
-    // message
-    public static string message = "Hello!" + DateTime.Now;
+    //return to Inbox tab
+    public const string RETURN_TO_INBOX_TAB = "//div[@data-tooltip='Входящие' or @data-tooltip='Inbox']";
 
-    
+    //// message
+    //public static string message = "Hello!" + DateTime.Now;
 
-    //update
-    public const string UPDATE_XPATH = "//*[@id=':4']/div/div[1]/div[1]/div/div/div[5]/div/div";
-    //*[@id="yDmH0d"]/c-wiz/div/div/div/div[2]/div[4]/div[1]/button  --- не сейчас
+    ////response message 
+    //public static string responseMessage = "Hi!" + DateTime.Now;
+
 }
